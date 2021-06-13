460107badf740e01eebca629c7081295ff3ad15a
parkhanseong <hs88610@gmail.com>
2021-06-13 11:54:36 +0900

    2차 완성

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,82 @@
 'use strict';
 
+$(function(){
+    /* ================= change theme ===================== */
+    $('#buttonDark').click(function(){
+        $(this).hide();
+        $('body').addClass('dark-theme');
+        $('#buttonLight').show();
+    })
+    $('#buttonLight').click(function(){
+        $('body').removeClass('dark-theme');
+        $('#buttonDark').show();
+        $(this).hide();
+    })
 
-// var swiper = new Swiper(".mySwiper", {});
+    /* ================= navbar ===================== */
+    var $navItem = $('.nav__item');
+    $navItem.click(function(e){
+        e.preventDefault();
+        var link = $(this).children('a').attr('data-link');
+        var $target = $(link);
+        var distance = $target.offset().top;
+        $('html, body').animate({scrollTop: distance});
+
+        $('.nav__container').removeClass('visible');
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+    })
+
+    /* nav toggle */
+    $('#navToggle, .nav__close').click(function(){
+        $('.nav__container').toggleClass('visible');
+    })
+
+    /* ================= skill tabs ================= */
+    $('.skills__header').click(function(){
+        $(this).children('.skill__icon--arrow').toggleClass('active');
+        $(this).siblings('.skill__data').stop().slideToggle();
+    })
+
+    /* ================= qualifictaion ================= */
+    var $qualificationTab = $('.qualification__tab');
+    var $qualificationContent = $('.qualification__content');
+    $qualificationContent.eq(1).hide();
+    $qualificationTab.click(function(e){
+        e.preventDefault();
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+        var idx = $(this).index();
+        $qualificationContent.hide();
+        $qualificationContent.eq(idx).show();
+    })
+
+    /* ================= service ================= */
+    $('.service__item-button').click(function(e){
+            e.preventDefault();
+            $(this).siblings('.service__modal').show();
+    })
+    $('.modal__close').click(function(){
+        $('.service__modal').hide();
+    })
+    $('.service__modal').click(function(e){
+        if(e.target === e.currentTarget){
+            $(this).hide();
+        }
+    })
+
+    /* ================= portfolio ================= */
+    $('.custom__slider').slick({
+        dots:true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+    /* ================= scroll up ================= */
+    
 
 
-const swiper = new Swiper('.mySwiper', {
-    // Optional parameters
-    // direction: ',
-    cssMode: true,
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
-  });
+})
+
+
+
